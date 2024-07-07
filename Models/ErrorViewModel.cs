f596360322cb4b27589a982f99acb5ab47bd8dab
candemiroguz <candemiroguz@outlook.com>
2024-07-08 00:36:23 +0300

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MappinKnow.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
