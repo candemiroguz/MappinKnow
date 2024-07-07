860fc25a9f8181872b0e2a1f8a27f7c6c6738ead
candemiroguz <candemiroguz@outlook.com>
2024-07-08 01:16:23 +0300

    Leaflet is integrated!

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,13 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+var map = L.map('map').setView([51.505, -0.09], 13);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '© OpenStreetMap contributors'
+}).addTo(map);
+
+map.on('click', function (e) {
+    var latlng = e.latlng;
+    alert("Coordinates: " + latlng.lat + ", " + latlng.lng);
+});
